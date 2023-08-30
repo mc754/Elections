78a239df18f97b12ec6d4ad0837ed7010ffcef94
mc754 <maxhchan@gmail.com>
2023-08-29 19:10:37 -0500

    test

---
+++ Trend-across-time.R
@@ -0,0 +1,36 @@
+# Trend across time for several pollsters
+library(tidyverse)
+library(dslabs)
+data("polls_us_election_2016")
+
+polls_us_election_2016 %>%
+  filter(state == "U.S." & enddate >= "2016-07-01") %>%
+  group_by(pollster) %>%
+  filter(n() >= 10) %>%
+  ungroup() %>%
+  mutate(spread = rawpoll_clinton/100 - rawpoll_trump/100) %>%
+  ggplot(aes(enddate, spread)) +
+  geom_smooth(method = "loess", span = 0.1) +
+  geom_point(aes(color = pollster), show.legend = FALSE, alpha = 0.6)
+
+#####################################################################################################
+
+# Plotting raw percentages across time
+library(tidyverse)
+library(dslabs)
+library(tidyr)
+data("polls_us_election_2016")
+
+polls_us_election_2016 %>%
+  filter(state == "U.S." & enddate >= "2016-07-01") %>%
+  select(enddate, pollster, rawpoll_clinton, rawpoll_trump) %>%
+  rename(Clinton = rawpoll_clinton, Trump = rawpoll_trump) %>%
+  gather(candidate, percentage, -enddate, -pollster) %>%
+  mutate(candidate = factor(candidate, levels = c("Trump", "Clinton"))) %>%
+  group_by(pollster) %>%
+  filter(n() >= 10) %>%
+  ungroup() %>%
+  ggplot(aes(enddate, percentage, color = candidate)) +
+  geom_point(show.legend = FALSE, alpha = 0.4) +
+  geom_smooth(method = "loess", span = 0.15) +
+  scale_y_continuous(limits = c(30, 50))
